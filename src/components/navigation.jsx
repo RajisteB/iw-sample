067f1e83cb5ceaaf0788311cd73950e9b906b0c3
Raj Bennin <rajisteb@gmail.com>
2018-05-22 22:11:09 -0400

    setup and navigation

---
+++ src/components/navigation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../images/logo.svg';
+import Burger from '../images/hamburger.png';
+import '../App.css';
+
+const navigation = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="logo"/>
+      </div>
+      <div className="nav-items">
+        <div className="item">212.555.5555</div>
+        <div className="item">LOGIN</div>
+        <div className="item">
+          <img src={Burger} alt="burger"/>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default navigation;
