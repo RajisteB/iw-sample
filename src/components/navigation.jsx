1fcb728ddf55241030bc6615a3465de1aad11a4c
Raj Bennin <rajisteb@gmail.com>
2018-05-23 10:06:12 -0400

    add media query for nav

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,17 +5,30 @@ import '../App.css';
 
 const navigation = () => {
   return (
-    <div className="navbar">
-      <div className="logo">
+    <div className="nav-container">
+      <div className="navbar row">
+        <div className="logo col">
+          <img src={Logo} alt="logo"/>
+        </div>
+        <div className="nav-items col">
+          <div className="item">212.555.5555</div>
+          <div className="item">LOG IN</div>
+          <div className="item">
+            <img src={Burger} alt="burger"/>
+          </div>
+        </div>
+      </div>
+      {/* <div className="logo col">
         <img src={Logo} alt="logo"/>
       </div>
-      <div className="nav-items">
+      <div className="break col">sdas</div>
+      <div className="nav-items col">
         <div className="item">212.555.5555</div>
         <div className="item">LOGIN</div>
         <div className="item">
           <img src={Burger} alt="burger"/>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 
