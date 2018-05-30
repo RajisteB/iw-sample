5847e9f036d603101a80b5094b4ce9f1d5835995
Raj Bennin <rajisteb@gmail.com>
2018-05-30 11:39:30 -0400

    refactor image paths

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
 import GridItem from './gridItem';
 import '../App.css';
+import Layer_01 from '../images/Layer_01.png';
+import Layer_02 from '../images/Layer_02.png';
+import Layer_03 from '../images/Layer_03.png';
+import Layer_04 from '../images/Layer_04.png';
+import Layer_05 from '../images/Layer_05.png';
+import Layer_06 from '../images/Layer_06.png';
 
 const data = [
   {
     id: 0,
+    image: Layer_01,
     date: 'January 12 2017',
     desc: 'Artisanal iconic cutting-edge business class.'
   },
   {
     id: 1,
+    image: Layer_02,
     date: 'January 18 2017',
     desc: 'K-pop extraordinary.'
   },
   {
     id: 2,
+    image: Layer_03,
     date: 'January 21 2017',
     desc: 'Esse airport veniam ryokan soft power.'
   },
   {
     id: 3,
+    image: Layer_04,
     date: 'January 27 2017',
     desc: 'St Moritz uniforms Beams.'
   },
   {
     id: 4,
+    image: Layer_05,
     date: 'February 2 2017',
     desc: 'Sharp bureaux sleepy K-pop carefully curated.'
   },
   {
     id: 5,
+    image: Layer_06,
     date: 'February 10 2017',
     desc: 'Bureaux exquisite delightful carefully curated soft power.'
   }
@@ -39,7 +51,7 @@ const Grid = () => {
   return (
     <div className="main-grid">
       {data.map((d, idx) => {
-        return <GridItem data={d} key={idx}/>
+        return <GridItem data={d} key={d.id}/>
       })}
     </div>
   )
--- src/components/gridItem.jsx
+++ src/components/gridItem.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import Layer01 from '../images/Layer01.png';
-import Layer02 from '../images/Layer02.png';
-import Layer03 from '../images/Layer03.png';
-import Layer04 from '../images/Layer04.png';
-import Layer05 from '../images/Layer05.png';
-import Layer06 from '../images/Layer06.png';
 
 const GridItem = (props) => {
-  let layers = null;
-  layers = [Layer01, Layer02, Layer03, Layer04, Layer05, Layer06].reverse();
   return (
     <div className="grid-item">
-      <img src={layers[props.data.id]} alt=""/>
+      <img src={props.data.image} alt=""/>
       <h4>{props.data.date}</h4>
       <h2>{props.data.desc}</h2>
       <h5>Presented By <a href="#">LOREM IPSUM</a></h5>
